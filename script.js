9a9446a01e910319ae858ee77e170336dbb36d65
huikaihoo <huikaihoo@gmail.com>
2020-01-24 10:15:50 +0800

    Fixed China source

--- script.js
+++ script.js
@@ -30,37 +30,37 @@ const modifyForHKMO = [[{ v: 'CN', f: '廣東' }, 0, 0]];
 const core = [['Region', '確診人數', '死亡人數']];
 
 const locationMap = {
-  '42': { v: 'CN-42', f: '湖北' },
-  '13': { v: 'CN-13', f: '河北' },
-  '53': { v: 'CN-53', f: '雲南' },
-  '35': { v: 'CN-35', f: '福建' },
-  '51': { v: 'CN-51', f: '四川' },
-  '37': { v: 'CN-37', f: '山東' },
-  '45': { v: 'CN-45', f: '廣西' },
-  '52': { v: 'CN-52', f: '貴州' },
-  '34': { v: 'CN-34', f: '安徽' },
-  '46': { v: 'CN-46', f: '海南' },
-  '64': { v: 'CN-64', f: '寧夏' },
-  '22': { v: 'CN-22', f: '吉林' },
-  '36': { v: 'CN-36', f: '江西' },
-  '12': { v: 'CN-12', f: '天津' },
-  '41': { v: 'CN-41', f: '河南' },
-  '50': { v: 'CN-50', f: '重慶' },
-  '14': { v: 'CN-14', f: '山西' },
-  '61': { v: 'CN-61', f: '陝西' },
-  '43': { v: 'CN-43', f: '湖南' },
-  '21': { v: 'CN-21', f: '遼寧' },
-  '11': { v: 'CN-11', f: '北京' },
-  '44': { v: 'CN-44', f: '廣東' },
-  '31': { v: 'CN-31', f: '上海' },
-  '33': { v: 'CN-33', f: '浙江' },
-  '23': { v: 'CN-23', f: '黑龍江' },
-  '32': { v: 'CN-32', f: '江蘇' },
-  '15': { v: 'CN-15', f: '內蒙古' },
-  '54': { v: 'CN-54', f: '西藏' },
-  '62': { v: 'CN-62', f: '甘肅' },
-  '63': { v: 'CN-63', f: '青海' },
-  '65': { v: 'CN-65', f: '新疆' },
+  湖北: { v: 'CN-42', f: '湖北' },
+  河北: { v: 'CN-13', f: '河北' },
+  云南: { v: 'CN-53', f: '雲南' },
+  福建: { v: 'CN-35', f: '福建' },
+  四川: { v: 'CN-51', f: '四川' },
+  山东: { v: 'CN-37', f: '山東' },
+  广西: { v: 'CN-45', f: '廣西' },
+  贵州: { v: 'CN-52', f: '貴州' },
+  安徽: { v: 'CN-34', f: '安徽' },
+  海南: { v: 'CN-46', f: '海南' },
+  宁夏: { v: 'CN-64', f: '寧夏' },
+  吉林: { v: 'CN-22', f: '吉林' },
+  江西: { v: 'CN-36', f: '江西' },
+  天津: { v: 'CN-12', f: '天津' },
+  河南: { v: 'CN-41', f: '河南' },
+  重庆: { v: 'CN-50', f: '重慶' },
+  山西: { v: 'CN-14', f: '山西' },
+  陕西: { v: 'CN-61', f: '陝西' },
+  湖南: { v: 'CN-43', f: '湖南' },
+  辽宁: { v: 'CN-21', f: '遼寧' },
+  北京: { v: 'CN-11', f: '北京' },
+  广东: { v: 'CN-44', f: '廣東' },
+  上海: { v: 'CN-31', f: '上海' },
+  浙江: { v: 'CN-33', f: '浙江' },
+  黑龙江: { v: 'CN-23', f: '黑龍江' },
+  江苏: { v: 'CN-32', f: '江蘇' },
+  内蒙古: { v: 'CN-15', f: '內蒙古' },
+  西藏: { v: 'CN-54', f: '西藏' },
+  甘肃: { v: 'CN-62', f: '甘肅' },
+  青海: { v: 'CN-63', f: '青海' },
+  新疆: { v: 'CN-65', f: '新疆' },
   MAC: { v: 'MO', f: '澳門' },
   HKG: { v: 'HK', f: '香港' },
   TWN: { v: 'TW', f: '台灣' },
@@ -70,21 +70,41 @@ const locationMap = {
   USA: { v: 'US', f: '美國' },
   SGP: { v: 'SG', f: '新加坡' },
   VNM: { v: 'VN', f: '越南' },
+  KSA: { v: 'SA', f: '沙特阿拉伯' },
 };
 
 const chinaConvert = record => {
-  const region = record.provinceId;
-  const str = record.tags;
+  const region = record.provinceShortName;
 
-  let confirm = 0,
+  let str = '',
+    confirm = 0,
     dead = 0;
 
-  if (str.indexOf('确诊') >= 0) {
+  if (record.tags && record.tags.indexOf('确诊') >= 0) {
+    str = record.tags;
+  } else if (record.comment && record.comment.indexOf('确诊') >= 0) {
+    str = record.comment;
+  } else if (record.confirmedCount) {
+    confirm = record.confirmedCount;
+  } else {
+    str = '';
+  }
+  if (str) {
     const regexp = /确诊([0-9 ]*)例/;
     let result = [...str.match(regexp)];
     confirm = parseInt(result[1].trim());
   }
-  if (str.indexOf('死亡') >= 0) {
+
+  if (record.comment && record.comment.indexOf('死亡') >= 0) {
+    str = record.comment;
+  } else if (record.tags && record.tags.indexOf('死亡') >= 0) {
+    str = record.tags;
+  } else if (record.deadCount) {
+    confirm = record.deadCount;
+  } else {
+    str = '';
+  }
+  if (str) {
     const regexp = /死亡([0-9 ]*)例/;
     let result = [...str.match(regexp)];
     dead = parseInt(result[1].trim());
@@ -153,8 +173,8 @@ const drawTable = () => {
   }
 
   for (let record of core) {
-    // console.log(record[0], record[0].v);
-    if (record[0].v && record[0].v.indexOf('CN-') !== 0) {
+    const regexp = /^[A-Za-z]+$/;
+    if (record[0].v && record[0].v.match(regexp)) {
       addRow(table, record);
       confirm += record[1];
       dead += record[2];
@@ -197,20 +217,28 @@ const drawRegionsMap = maxValue => {
   chart.draw(dataWorld, { ...options, resolution: 'auto', region: 'SG' });
 };
 
-const request = new XMLHttpRequest();
-
+let retry = 0;
+let request = new XMLHttpRequest();
 request.open('GET', 'https://cors-anywhere.herokuapp.com/https://3g.dxy.cn/newh5/view/pneumonia');
 request.onload = function() {
   if (request.readyState === 4 && request.status === 200) {
-    const regexp = /getListByCountryTypeService1 = (.*)\}catch(.*)get/;
+    const regexp = /getAreaStat = (.*)\}catch(.*)get/;
     const str = request.responseText;
     // console.log(str);
     let result = str.match(regexp);
-    let dataStr = result[1].substr(0, result[1].indexOf(']') + 1);
-    // console.log(dataStr);
-    chinaData = JSON.parse(dataStr);
-    console.log('china', chinaData);
-    showMap();
+
+    if (result) {
+      let dataStr = result[1].substr(0, result[1].indexOf(']}catch(e)') + 1);
+      // console.log(dataStr);
+      chinaData = JSON.parse(dataStr);
+      console.log('china', chinaData);
+      showMap();
+    } else {
+      retry++;
+      console.log('retry', retry);
+      request.open('GET', 'https://cors-anywhere.herokuapp.com/https://3g.dxy.cn/newh5/view/pneumonia');
+      request.send(null);
+    }
   }
 };
 request.send(null);
